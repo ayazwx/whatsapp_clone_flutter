d301b6e473e37947f4a843e191e286a4ed6b4ae1
ayazwx <vefaayaz0404@gmail.com>
2022-10-14 12:06:31 +0300

    Camera Page was created

--- lib/view/camera_page.dart
+++ lib/view/camera_page.dart
@@ -10,6 +10,37 @@ class CameraPage extends StatefulWidget {
 class _CameraPageState extends State<CameraPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.white,
+          child: const Icon(
+            Icons.circle_outlined,
+            size: 50,
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        body: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            verticalDirection: VerticalDirection.up,
+            children: <Widget>[
+              Container(
+                  margin: const EdgeInsets.only(right: 10, top: 5),
+                  height: 20.0,
+                  width: 20.0,
+                  child: FittedBox(
+                    child: FloatingActionButton(
+                      mini: true,
+                      onPressed: () {},
+                      backgroundColor: Colors.white,
+                      child: const Icon(
+                        Icons.flash_on,
+                        size: 30,
+                      ),
+                    ),
+
+                    //const FloatingActionButtonLocation(),
+                  )),
+            ]));
   }
 }
