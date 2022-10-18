c6990df0ad5cd164fcca681936cb7c056109012f
ayazwx <vefaayaz0404@gmail.com>
2022-10-18 23:21:46 +0300

    implemented pop function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:mywhatsapp/view/home_screen.dart';
 
-void main() {
+late List<CameraDescription> cameras;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(const MyApp());
 }
 
--- lib/view/status_page.dart
+++ lib/view/status_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mywhatsapp/view/chats_page.dart';
+import 'package:mywhatsapp/view/home_screen.dart';
 
 class StatusPage extends StatefulWidget {
   const StatusPage({Key? key}) : super(key: key);
@@ -8,79 +10,89 @@ class StatusPage extends StatefulWidget {
 }
 
 class _StatusPageState extends State<StatusPage> {
+  pop() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Homescreen(),
+        ));
+  }
   // late List<String> Status;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: ListView.builder(
-          itemBuilder: (context, index) {
-            return SizedBox(
-              height: 70,
-              child: Row(
-                children: [
-                  Stack(children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.only(left: 1),
-                      width: 50,
-                      height: 50,
-                      child: Image.network(
-                          'https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png'),
-                    ),
-                    Container(
-                        margin: const EdgeInsets.only(left: 30, top: 30),
-                        height: 20.0,
-                        width: 20.0,
-                        child: FittedBox(
-                          child: FloatingActionButton(
-                            mini: true,
-                            onPressed: () {},
-                            backgroundColor: Colors.green,
-                            child: const Icon(
-                              Icons.add,
-                              size: 30,
+    return WillPopScope(
+      onWillPop: () async => pop(),
+      child: Scaffold(
+          body: ListView.builder(
+            itemBuilder: (context, index) {
+              return SizedBox(
+                height: 70,
+                child: Row(
+                  children: [
+                    Stack(children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.only(left: 1),
+                        width: 50,
+                        height: 50,
+                        child: Image.network(
+                            'https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png'),
+                      ),
+                      Container(
+                          margin: const EdgeInsets.only(left: 30, top: 30),
+                          height: 20.0,
+                          width: 20.0,
+                          child: FittedBox(
+                            child: FloatingActionButton(
+                              mini: true,
+                              onPressed: () {},
+                              backgroundColor: Colors.green,
+                              child: const Icon(
+                                Icons.add,
+                                size: 30,
+                              ),
                             ),
-                          ),
 
-                          //const FloatingActionButtonLocation(),
-                        ))
-                  ]),
+                            //const FloatingActionButtonLocation(),
+                          ))
+                    ]),
 
-                  // const Spacer(
-                  //   flex: 50,
-                  // ),
+                    // const Spacer(
+                    //   flex: 50,
+                    // ),
 
-                  // Container(
-                  //   child: const Text('Viewed updates'),
-                  // ) // const Divider(),
-                ],
+                    // Container(
+                    //   child: const Text('Viewed updates'),
+                    // ) // const Divider(),
+                  ],
+                ),
+              );
+            },
+            itemCount: 1,
+          ),
+          floatingActionButton:
+              Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+            FloatingActionButton(
+              mini: true,
+              onPressed: () {},
+              backgroundColor: Colors.green,
+              child: const Icon(
+                Icons.create,
+                size: 20,
               ),
-            );
-          },
-          itemCount: 1,
-        ),
-        floatingActionButton:
-            Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-          FloatingActionButton(
-            mini: true,
-            onPressed: () {},
-            backgroundColor: Colors.green,
-            child: const Icon(
-              Icons.create,
-              size: 20,
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: Colors.green,
-            child: const Icon(
-              Icons.camera_alt,
-              size: 20,
+            const SizedBox(
+              height: 10,
             ),
-          ),
-        ]));
+            FloatingActionButton(
+              onPressed: () {},
+              backgroundColor: Colors.green,
+              child: const Icon(
+                Icons.camera_alt,
+                size: 20,
+              ),
+            ),
+          ])),
+    );
   }
 }
