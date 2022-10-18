6cf14ba74c21f84d30d1c39b1d8ae3c4c8292747
ayazwx <vefaayaz0404@gmail.com>
2022-10-18 23:19:22 +0300

    imported pop function

--- lib/view/calls_page.dart
+++ lib/view/calls_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_screen.dart';
+
 class CallsPage extends StatefulWidget {
   const CallsPage({Key? key}) : super(key: key);
 
@@ -8,46 +10,55 @@ class CallsPage extends StatefulWidget {
 }
 
 class _CallsPageState extends State<CallsPage> {
+  pop() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Homescreen(),
+        ));
+  }
   // late List<String> Calls;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: ListView.builder(
-          itemBuilder: (context, index) {
-            return SizedBox(
-              height: 70,
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 10,
-                    child: Image.network(
-                        'https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png'),
+    return WillPopScope(
+        onWillPop: () async => pop(),
+        child: Scaffold(
+            body: ListView.builder(
+              itemBuilder: (context, index) {
+                return SizedBox(
+                  height: 70,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 10,
+                        child: Image.network(
+                            'https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png'),
+                      ),
+                      const Spacer(
+                        flex: 30,
+                      ),
+                      const Expanded(flex: 20, child: Icon(Icons.call)),
+                      const Expanded(flex: 10, child: Icon(Icons.videocam)),
+                      // const Divider(),
+                    ],
                   ),
-                  const Spacer(
-                    flex: 30,
+                );
+              },
+              itemCount: 15,
+            ),
+            floatingActionButton: SizedBox(
+                height: 50.0,
+                width: 50.0,
+                child: FittedBox(
+                  child: FloatingActionButton(
+                    onPressed: () {},
+                    backgroundColor: Colors.green,
+                    child: const Icon(
+                      Icons.call,
+                      size: 20,
+                    ),
                   ),
-                  const Expanded(flex: 20, child: Icon(Icons.call)),
-                  const Expanded(flex: 10, child: Icon(Icons.videocam)),
-                  // const Divider(),
-                ],
-              ),
-            );
-          },
-          itemCount: 15,
-        ),
-        floatingActionButton: SizedBox(
-            height: 50.0,
-            width: 50.0,
-            child: FittedBox(
-              child: FloatingActionButton(
-                onPressed: () {},
-                backgroundColor: Colors.green,
-                child: const Icon(
-                  Icons.call,
-                  size: 20,
-                ),
-              ),
-            )));
+                ))));
   }
 }
